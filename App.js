d0772016c6a362440afaa953dc1e56fe25f79448
sanghavikartigayan <sanghavikartigayan@gmail.com>
2020-03-01 14:25:20 -0500

    start page

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './Components/Header';
+import StartGameScreen from './Screens/StartGameScreen';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ screen: {
+   flex: 1
+ }
+});
